4153f4b8a575f1c8dcf6a90735545b93e220521d
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-09-22 19:46:59 +0100

    using a std Java 19

--- java/generate-sources.sh
+++ java/generate-sources.sh
@@ -10,4 +10,7 @@ if [ "$(uname)" == "Darwin" ]; then
   EXTRA_INCLUDE="-I /Library/Developer/CommandLineTools/usr/include/c++/v1 -I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
 fi
 
-jextract ${EXTRA_INCLUDE} --source -t tech.jotto --include-function hello_world -l jotto -d ${OUTPUT_DIR} -- ${SCRIPT_DIR}/../native/lib.h
+# using the bundled
+# jextract ${EXTRA_INCLUDE} --source -t tech.jotto --include-function hello_world -l jotto -d ${OUTPUT_DIR} -- ${SCRIPT_DIR}/../native/lib.h
+# compiled from source
+jextract ${EXTRA_INCLUDE} --source -t tech.jotto --include-function hello_world -l jotto --output ${OUTPUT_DIR} ${SCRIPT_DIR}/../native/lib.h
